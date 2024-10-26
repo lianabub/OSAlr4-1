0ea95fd47a27f77e0e221b2d20ade4f2c1b4926e
liana <lianabub07@gmail.com>
2024-10-26 09:58:34 +0300

    Add files via upload

---
+++ lr4-1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	setlocale(LC_ALL, "ru");
+	int n, k;
+	cout << "Введите начальное число: ";
+	cin >> n;
+	cout << "Введите конечное число";
+	cin >> k;
+	for (n; n > 0 && n < k && k < 1000; n++)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			cout << n << endl;
+		}
+	}
+	return 0;
+}
